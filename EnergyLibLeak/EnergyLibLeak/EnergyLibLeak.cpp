0b92b83b5b8b4e567f955dc2d588fd658e0be859
Alexander Riccio <alexander@riccio.com>
2015-10-07 23:09:53 -0400

    formatting tweaks

--- EnergyLibLeak/EnergyLibLeak/EnergyLibLeak.cpp
+++ EnergyLibLeak/EnergyLibLeak/EnergyLibLeak.cpp
@@ -7,17 +7,17 @@
 #include <Windows.h>
 #include <cstdio>
 
+PCWSTR const dllName = 
 #if _M_X64
-PCWSTR const dllName = L"\\EnergyLib64.dll";
+	L"\\EnergyLib64.dll";
 #else
-PCWSTR const dllName = L"\\EnergyLib32.dll";
+	L"\\EnergyLib32.dll";
 #endif
 
 typedef _Return_type_success_(return != 0) int POWER_GADGET_SUCCESS;
 typedef POWER_GADGET_SUCCESS(*IntelEnergyLibInitialize_t)();
 
-void printLastError(const DWORD lastErr = ::GetLastError( ))
-{
+void printLastError(const DWORD lastErr = ::GetLastError()) {
 	const DWORD errMsgSize = 1024u;
 	wchar_t errBuff[errMsgSize] = {0};
 	const DWORD ret = ::FormatMessageW(
@@ -27,22 +27,19 @@ void printLastError(const DWORD lastErr = ::GetLastError( ))
 	if (ret == 0)
 		std::terminate();//FormatMessageW failed.
 	wprintf_s(L"Encountered an error: %s\r\n", errBuff);
-}
+	}
 
-std::wstring GetEnvironmentVariableString(_In_z_ PCWSTR const variable)
-{
+std::wstring GetEnvironmentVariableString(_In_z_ PCWSTR const variable) {
 	const rsize_t bufferSize = 512u;
 	wchar_t buffer[bufferSize] = {0};
 	rsize_t sizeRequired = 0u;
 	const errno_t result = _wgetenv_s(&sizeRequired, buffer, variable);
 	if (result == 0)
 		return buffer;
-	wprintf_s( 
-		L"_wgetenv_s %s, sizeRequired is: %i64u\r\n",
-		((result == ERANGE) ? L"returned ERANGE, " : L"failed, but returned a value other than ERANGE"),
-		result);
+	wprintf_s( L"_wgetenv_s %s\r\n",
+		((result == ERANGE) ? L"returned ERANGE" : L"failed, but returned a value other than ERANGE"));
 	return L"";
-}
+	}
 
 HMODULE loadLib( ) {
 	const std::wstring IPG_DIR( GetEnvironmentVariableString( L"IPG_Dir" ) );
@@ -50,52 +47,50 @@ HMODULE loadLib( ) {
 	HMODULE EnergyLib = ::LoadLibraryW( EnergyLibDll.c_str( ) );
 	if ( EnergyLib == NULL ) {
 		wprintf_s( L"Failed to load DLL!\r\n" );
-		printLastError( );
-		abort();
+		printLastError();
+		std::terminate();
 		}
 	return EnergyLib;
 	}
 
-FARPROC getFuncAddr( HMODULE EnergyLib, _In_z_ PCSTR const funcName ) {
+FARPROC getFuncAddr(HMODULE EnergyLib, _In_z_ PCSTR const funcName) {
 	FARPROC FuncAddr = ::GetProcAddress( EnergyLib, funcName );
 	if ( FuncAddr == NULL ) {
 		wprintf_s( L"Failed to load find address of %S!\r\n", funcName );
-		printLastError( );
-		abort();
+		printLastError();
+		std::terminate();
 		}
 	return FuncAddr;
 	}
 
-IntelEnergyLibInitialize_t initialize( HMODULE EnergyLib ) {
+IntelEnergyLibInitialize_t initialize(HMODULE EnergyLib) {
 	const IntelEnergyLibInitialize_t IntelEnergyLibInitialize =
 		reinterpret_cast<IntelEnergyLibInitialize_t>( getFuncAddr( EnergyLib, "IntelEnergyLibInitialize" ) );
 	const POWER_GADGET_SUCCESS Initialized = ( *IntelEnergyLibInitialize )( );
 	if ( !Initialized ) {
 		wprintf_s( L"IntelEnergyLibInitialize failed!\r\n" );
-		abort();
+		std::terminate();
 		}
 	return IntelEnergyLibInitialize;
 	}
 
-
-void freeLib( _Pre_valid_ _Post_ptr_invalid_ HMODULE lib ) {
+void freeLib(_Pre_valid_ _Post_ptr_invalid_ HMODULE lib) {
 	//"\\.\EnergyDriver" is leaked here.
 	const BOOL freed = ::FreeLibrary( lib );
 	if ( freed == 0 ) {
-		printLastError( );
-		abort( );
+		printLastError();
+		std::terminate();
 		}
 	}
 
-
-void doEnergyLib( ) {
+void doEnergyLib() {
 	const HMODULE EnergyLib = loadLib( );
 	initialize( EnergyLib );
 	freeLib( EnergyLib );
 	}
 
-int main( ) {
-	const size_t maxEnergyLib = 100u;
+int main() {
+	const size_t maxEnergyLib = 1000000000u;
 	for ( size_t i = 0u; i < maxEnergyLib; ++i ) {
 		doEnergyLib( );
 		}
